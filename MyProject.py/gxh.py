4f49c120cfcb46a2611d863f896e396eae7498c4
Raj Patel <119167972+Captain-Raj@users.noreply.github.com>
2023-02-06 15:22:14 +0530

    Delete MyProject.py directory

--- MyProject.py/gxh.py
+++
@@ -1,9 +0,0 @@
-x=int(input("please provide any input or 0 to terminate\n"))
-while x!=0:
-	if x%2==0:
-		print(x, "is an even number")
-	else:
-		print(x, " is an odd number")
-	x = int(input("please provide any input or 0 to terminate\n"))
-	if x==0:
-		print("You entered 0, program will exit now.")
--- MyProject.py/yash.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-engine=pyttsx3.init('sapi5')
-voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
